1a40d3458484a32019156710b8b403b650d8244d
Am10aN16 <2020071013@mmmut.ac.in>
2022-09-09 17:33:28 +0530

    2nd

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const express = require("express");
 const dotenv = require("dotenv")
 const app = express();
-
+const cors = require("cors")
+const fileUpload = require("express-fileupload");
 const PORT = 5000;
+dotenv.config({ path: './.env' });
+const dbconnection = require("./db/conn");
 
-
-
-dotenv.config({ path: './config.env' });
-require("./db/conn");
+app.use(cors());
+app.use(fileUpload({
+    useTempFiles: true
+}));
 
 
 app.use(express.json());
+app.use('/api' , require('./routes/upload'));
 
 app.get("/" , (req, res)=>{
     res.send("welcome to the server side");
 })
 
 
+dbconnection().then(()=>{
+    app.listen(PORT , ()=>{
+        console.log(`Server is listening to port ${PORT}`);
+    });
+})
 
-app.listen(PORT , ()=>{
-    console.log(`Server is listening to port ${PORT}`);
-});
